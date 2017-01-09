7f46b404d43b1323d3d8c44d2636cfb0d9c0a588
Alvin <alvin@igaworks.com>
2017-01-09 18:22:41 +0900

    Chapter04 get set

--- Chapter04/LanguageFeatures/Controllers/HomeController.cs
+++ Chapter04/LanguageFeatures/Controllers/HomeController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
 	public class HomeController : Controller
 	{
 		// GET: Home
-		public ActionResult Index()
+		public string Index()
 		{
-			return View();
+			return "Navigate to a URL to show an example.";
+		}
+
+		public ViewResult AutoProperty()
+		{
+			Product myProduct = new Product();
+
+			myProduct.Name = "Kayak";
+
+			string productName = myProduct.Name;
+
+			return View("Result", (object)String.Format("Product name: {0}", productName));
 		}
 	}
 }
